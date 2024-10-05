9d8647a584709b46434cd474b14de70660683caf
K <51281148+K-dash@users.noreply.github.com>
2024-10-05 20:48:35 +0000

    ADd git ignore

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,3 @@
+
+dotnet restore
+dotnet dev-certs https --clean &&dotnet dev-certs https --trust
--- BlazorAzureADB2CApp1/Program.cs
+++ BlazorAzureADB2CApp1/Program.cs
@@ -1,10 +1,40 @@
 using BlazorAzureADB2CApp1.Components;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//★(a) Configure Azure AD B2C Authentication using OpenID Connect
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAdB2C"))
+    .EnableTokenAcquisitionToCallDownstreamApi()
+    .AddInMemoryTokenCaches();
+
+//★(b) Add authorization policy requiring authentication for all controllers
+builder.Services.AddControllersWithViews(options =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+    options.Filters.Add(new AuthorizeFilter(policy));
+});
+
+//★(c) Add Razor Pages and Microsoft Identity UI for authentication-related pages
+builder.Services.AddRazorPages()
+    .AddMicrosoftIdentityUI();
+
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddLocalization();
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("ja-JP");
+CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("ja-JP");
 
 var app = builder.Build();
 
@@ -23,5 +53,9 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+//★(d) Enable routing for authentication-related UI pages
+app.MapControllers();
+app.MapRazorPages();
 
 app.Run();
